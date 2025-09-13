4436f7aa3a895399d482e570db7cc2c3197dcd8b
Ian Hinder <ian.hinder@manchester.ac.uk>
2025-09-13 12:20:05 +0100

    Don't concretize in setup.sh

--- etc/spack/setup.sh
+++ etc/spack/setup.sh
@@ -14,6 +14,6 @@ else
 fi
 
 spack env activate $envname
-spack concretize --force # this is a bit dangerous, but needed at the moment
+#spack concretize --force # this is a bit dangerous, but needed at the moment
 spack install
 spack module tcl refresh -y
