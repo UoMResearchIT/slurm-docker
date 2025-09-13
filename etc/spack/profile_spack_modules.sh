82c64a47113bfbb9ba3eac67e3a69fb091173c46
Ian Hinder <ian.hinder@manchester.ac.uk>
2025-09-13 09:59:37 +0100

    Update paths in module setup for x86_64

--- etc/spack/profile_spack_modules.sh
+++ etc/spack/profile_spack_modules.sh
@@ -1,2 +1,2 @@
-source /cluster/software/linux-m1/environment-modules-5.5.0-6iu4zsbsmuzmmjmu4eodobrff2mxegx7/init/bash
-export MODULEPATH=/cluster/software/modules/linux-ubuntu20.04-m1:$MODULEPATH
+source /cluster/software/linux-x86_64/environment-modules-*/init/bash
+export MODULEPATH=/cluster/software/modules/linux-ubuntu20.04-x86_64:$MODULEPATH
